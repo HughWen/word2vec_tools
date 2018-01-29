202f5c9c39258d8ba23025c4c6487e5741d9e9d1
ruoningwen <ruoningwen@gmail.com>
2018-01-29 16:48:52 +0800

    update train w2v model

--- train_word2vec_model.py
+++ train_word2vec_model.py
@@ -9,6 +9,7 @@ from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 
 
+
 if __name__ == '__main__':
     program = os.path.basename(sys.argv[0])
     logger = logging.getLogger(program)
@@ -19,14 +20,16 @@ if __name__ == '__main__':
 
     # check and process input arguments
     if len(sys.argv) < 4:
-        print globals()['__doc__'] % locals()
+        print(globals()['__doc__'] % locals())
         sys.exit(1)
     inp, outp1, outp2 = sys.argv[1:4]
 
-    model = Word2Vec(LineSentence(inp),sg=1, size=400, window=5, min_count=5,iter=5,
+    model = Word2Vec(LineSentence(inp),sg=1, size=200, hs=1, window=5, min_count=5, iter=50,
             workers=multiprocessing.cpu_count())
 
     # trim unneeded model memory = use(much) less RAM
     #model.init_sims(replace=True)
     model.save(outp1)
-    model.save_word2vec_format(outp2, binary=False)
+    model.wv.save_word2vec_format(outp2, binary=False)
+
+
